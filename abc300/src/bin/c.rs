b56badcc533bb6978ec8a9a6f5e2f2c1c2b88246
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2023-05-20 12:06:52 +0900

    implemented abc300 c

--- abc300/src/bin/c.rs
+++ abc300/src/bin/c.rs
@@ -4,17 +4,86 @@
 // use std::collections::BinaryHeap;
 // use proconio::marker::Chars;
 use proconio::input;
-use std::collections::HashMap;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-        n: usize,
-        a: [usize; n],
+        (h,w): (usize, usize),
+        c: [Chars; h],
     }
-    let mut map = HashMap::new();
+    let n = std::cmp::min(h,w);
+    let mut s = vec![0; n];
+    let mut centers = vec![];
+    for i in 1..h-1 {
+        for j in 1..w-1 {
+            if
+                c[i][j] == '#' &&
+                c[i+1][j+1] == '#' &&
+                c[i+1][j-1] == '#' &&
+                c[i-1][j+1] == '#' &&
+                c[i-1][j-1] == '#' {
+                centers.push((i,j));
+            }
+        }
+    }
+    for i in 0..centers.len() {
+        let mut size = 1;
+        while
+            centers[i].0 + size < h &&
+            centers[i].1 + size < w &&
+            c[centers[i].0 + size][centers[i].1 + size] == '#' {
+
+            size += 1
+        }
+        s[size-2] += 1;
+    }
+
     for i in 0..n {
-        *map.entry(a[i]).or_insert(0) += 1;
+        if i == n-1 {
+            print!("{}\n", s[i]);
+        } else {
+            print!("{} ", s[i]);
+        }
     }
-    let ans = map.values().fold(0, |sum, x| sum + x/2);
-    println!("{}", ans);
 }
+
+// fn main() {
+//     input! {
+//         (h,w): (usize, usize),
+//         c: [Chars; h],
+//     }
+//     let mut state = vec![vec![false; w]; h];
+//     let n = std::cmp::min(h,w);
+//     let mut s = vec![0; n];
+
+//     for i in 0..h {
+//         for j in 0..w {
+//             if c[i][j] == '#' && state[i][j] == false {
+//                 let mut len = 0;
+//                 {
+//                     let (mut k, mut l) = (i,j);
+//                     while k < h && l < w && c[k][l] == '#'{
+//                         k += 1;
+//                         l += 1;
+//                         len += 1;
+//                     }
+//                 }
+//                 let size = len/2;
+//                 s[size-1] += 1;
+
+//                 let center = (i+size, j + size);
+//                 state[center.0][center.1] = true;
+//                 for k in 1..=size {
+//                     state[center.0+k][center.1+k] = true;
+//                     state[center.0-k][center.1+k] = true;
+//                     state[center.0+k][center.1-k] = true;
+//                     state[center.0-k][center.1-k] = true;
+//                 }
+//             }
+//         }
+//     }
+//     for i in 0..n-1 {
+//         print!("{} ", s[i]);
+//     }
+//     print!("{}", s[n-1]);
+// }
