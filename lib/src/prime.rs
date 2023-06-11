7974e7ee807a747a250b4c37d7c8f31258f2e175
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2023-06-11 17:39:08 +0900

    added get_primes()

---
+++ lib/src/prime.rs
@@ -0,0 +1,15 @@
+/// 約数一覧を取得
+fn get_primes(n: usize) -> Vec<usize> {
+    let mut i = 1;
+    let mut ret = vec![];
+    while i * i <= n {
+        if i * i == n {
+            ret.push(i);
+        } else if n % i == 0 {
+            ret.push(i);
+            ret.push(n/i);
+        }
+        i += 1;
+    }
+    ret
+}
