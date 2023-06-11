7801e2d80a524b94eb3fc1ba9da4aa4dd8932f5e
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2023-06-11 15:10:57 +0900

    implemented abc204-d

--- abc204/src/bin/d.rs
+++ abc204/src/bin/d.rs
@@ -1,14 +1,36 @@
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use std::collections::BinaryHeap;
-// use proconio::marker::Chars;
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
         t: [usize; n],
+    }
+    let s = t.iter().sum::<usize>();
+    let mut dp = vec![vec![false; s+1]; n+1];
+    dp[0][0] = true;
+    for i in 1..n+1 {
+        for j in 0..s+1 {
+            if dp[i-1][j] {
+                dp[i][j] = true;
+            }
+            if j < t[i-1] {
+                //
+            } else if dp[i-1][j-t[i-1]] {
+                dp[i][j] = true;
+            }
+        }
+    }
+    let lower = if s % 2 == 1 {
+        s/2 + 1
+    } else {
+        s/2
+    };
+
+    for i in lower..s+1 {
+        if dp[n][i] {
+            println!("{}", i);
+            break;
+        }
     }
 }
 
