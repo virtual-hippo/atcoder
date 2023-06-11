cfef00b9a56ff4b0c23b31ad5c585e9a3e65dd1d
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2023-06-11 18:25:11 +0900

    added bfs

---
+++ lib/src/bfs.rs
@@ -0,0 +1,18 @@
+use std::collections::VecDeque;
+
+fn bfs(graph: &Vec<Vec<usize>>, start: usize) -> Vec<i64> {
+    let mut dist = vec![-1_i64; graph.len()];
+    let mut queue = VecDeque::new();
+    queue.push_back(1);
+    dist[start] = 0;
+    while queue.is_empty() == false {
+        let pos = queue.pop_front().unwrap();
+        for to in graph[pos].iter() {
+            if dist[*to] == -1 {
+                dist[*to] = dist[pos] + 1;
+                queue.push_back(*to);
+            }
+        }
+    }
+    dist
+}
