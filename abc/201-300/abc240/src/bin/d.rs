fdf9d61a0804e5ba44c9badd31e5986323f4dfe8
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2023-07-01 15:27:52 +0900

    implemented abc240-d

--- abc/201-300/abc240/src/bin/d.rs
+++ abc/201-300/abc240/src/bin/d.rs
@@ -8,10 +8,39 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        // (h,w): (usize, usize),
-        // s: Chars,
-        // a: [usize; h],
+        a: [usize; n],
     }
-    println!("Yes");
+    let mut vec: Vec<(usize, usize)> = Vec::with_capacity(n);
+    let mut prev = 0;
+    let mut cnt = 0;
+    for i in 0..n {
+        cnt += 1;
+        let k = a[i];
+        let tail = if vec.len() == 0 {
+            0
+        } else {
+            vec.len()-1
+        };
+        if k != prev {
+            vec.push((k, 1));
+            prev = k;
+        } else {
+            vec[tail].1 += 1;
+            if vec[tail].1 >= k {
+                vec[tail].1 -= k;
+                cnt -= k;
+                if vec[tail].1 == 0 {
+                    vec.pop();
+                    if vec.len() == 0 {
+                        prev = 0;
+                    } else {
+                        let new_tail = vec.len()-1;
+                        prev = vec[new_tail].0;
+                    }
+                }
+            }
+        }
+        println!("{}", cnt);
+    }
 }
 
