1118f60bb130c5f741418e976106e864d8f75770
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2023-07-08 16:38:03 +0900

    implemented abc268-c

--- abc/201-300/abc268/src/bin/c.rs
+++ abc/201-300/abc268/src/bin/c.rs
@@ -1,34 +1,15 @@
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use std::collections::BinaryHeap;
-// use proconio::marker::Chars;
 use proconio::input;
-use std::collections::VecDeque;
 
 fn main() {
     input! {
         n: usize,
-        p: [i64; n],
+        p: [usize; n],
     }
-    let n_i64 = n as i64;
-    let mut deq = VecDeque::from(p);
-    let mut ans = 0;
-    for ii in &[n_i64-1, 0, 1] {
-        let mut current = 0;
-        while deq[0] != *ii {
-            let val = deq.pop_back().unwrap();
-            deq.push_front(val);
+    let mut cnt = vec![0; n];
+    for i in 0..n {
+        for j in 0..3 {
+            cnt[(n + p[i] - 1 + j - i) as usize % n] += 1;
         }
-        for i in 0..n {
-            let idx = i as i64;
-            if idx -1 <= deq[i] && deq[i] <= idx + 1 {
-                current += 1;
-            }
-        }
-        ans = std::cmp::max(ans, current);
     }
-    
-    println!("{}", ans);
+    println!("{}", cnt.iter().max().unwrap());
 }
-
