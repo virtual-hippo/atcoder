9f27d4c8874e3810b47befb30f0848061abe8363
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2023-07-08 20:09:54 +0900

    implemented abc234-c,d

--- abc/201-300/abc234/src/bin/c.rs
+++ abc/201-300/abc234/src/bin/c.rs
@@ -9,14 +9,8 @@ fn main() {
     input! {
         k: u64,
     }
-    // 2,20,22,200,202,220,222,2000,2002,2020,2022,2200,2202,2220,2222
-    let mut i = 0;
-    let mut current = 0_u64;
-    while current < k {
-        current += 2_u64.pow(i);
-        i += 1;
-    }
-    println!("{}", (current+1)/2);
-    println!("{}", 2 * 10_u64.pow(i-1));
+    let k_str = format!("{:>b}", k);
+    let ans: String = k_str.chars().map(|ch| if ch == '1' {'2'} else {'0'}).collect();
+    println!("{}", ans);
 }
 
--- abc/201-300/abc234/src/bin/d.rs
+++ abc/201-300/abc234/src/bin/d.rs
@@ -3,15 +3,24 @@
 // use std::collections::VecDeque;
 // use std::collections::BinaryHeap;
 // use proconio::marker::Chars;
+use std::collections::BinaryHeap;
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        // (h,w): (usize, usize),
-        // s: Chars,
-        // a: [usize; h],
+        k: usize,
+        p: [i64; n],
     }
-    println!("Yes");
-}
+    let mut b_heap = BinaryHeap::with_capacity(k+1);
+    for i in 0..k {
+        b_heap.push(-p[i]);
+    }
+    println!("{}", -1 * *(b_heap.peek().unwrap()));
 
+    for i in k..n {
+        b_heap.push(-p[i]);
+        b_heap.pop();
+        println!("{}", -1 * *(b_heap.peek().unwrap()));
+    }
+}
