1ccfa97b8d7b1668b1f06e3cdcff85fb81cf9421
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2023-07-09 11:02:41 +0900

    fixed bfs() at lib

--- lib/src/bfs.rs
+++ lib/src/bfs.rs
@@ -3,7 +3,7 @@ use std::collections::VecDeque;
 fn bfs(graph: &Vec<Vec<usize>>, start: usize) -> Vec<i64> {
     let mut dist = vec![-1_i64; graph.len()];
     let mut queue = VecDeque::new();
-    queue.push_back(1);
+    queue.push_back(start);
     dist[start] = 0;
     while queue.is_empty() == false {
         let pos = queue.pop_front().unwrap();
