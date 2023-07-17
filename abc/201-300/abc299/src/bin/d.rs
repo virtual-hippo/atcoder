712e6bd6d567cb27281369651012169171291051
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2023-07-17 19:24:02 +0900

    implemented abc299-d

--- abc/201-300/abc299/src/bin/d.rs
+++ abc/201-300/abc299/src/bin/d.rs
@@ -1,17 +1,31 @@
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use std::collections::BinaryHeap;
-// use proconio::marker::Chars;
 use proconio::input;
+use proconio::source::line::LineSource;
+use std::io::{self, BufReader};
 
 fn main() {
+    let mut stdin = LineSource::new(BufReader::new(io::stdin()));
+    macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut stdin, $($tt)*)));
+
     input! {
         n: usize,
-        // (h,w): (usize, usize),
-        // s: Chars,
-        // a: [usize; h],
     }
-    println!("Yes");
+
+    let (mut left, mut right) = (1, n);
+
+    let mut i = 0;
+    while right - left > 1 && i < 20 {
+        let mid = (left + right) / 2;
+        println!("? {}", mid);
+        input! {
+            si: i32,
+        }
+        if si == 0 {
+            left = mid;
+        } else {
+            right = mid;
+        }
+        i += 1;
+    }
+    println!("! {}", left);
 }
 
