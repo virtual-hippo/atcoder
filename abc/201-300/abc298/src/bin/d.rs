869aaab630696f89a4ef8e5ed552db3564dda613
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2023-07-17 23:33:39 +0900

    implemented abc298-d

--- abc/201-300/abc298/src/bin/d.rs
+++ abc/201-300/abc298/src/bin/d.rs
@@ -1,17 +1,47 @@
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use std::collections::BinaryHeap;
-// use proconio::marker::Chars;
 use proconio::input;
+use std::collections::VecDeque;
 
 fn main() {
     input! {
-        n: usize,
-        // (h,w): (usize, usize),
-        // s: Chars,
-        // a: [usize; h],
+        q: usize,
     }
-    println!("Yes");
-}
+    const MOD: u64 = 998244353;
 
+    let mut mods = vec![1; q+1];
+
+    for i in 2..q+1 {
+        mods[i] = 10 * mods[i-1] % MOD;
+    }
+
+    let mut s = VecDeque::with_capacity(q);
+    s.push_back('1');
+    let mut current = 1;
+
+
+    for _ in 0..q {
+        input! {
+            query: usize,
+        }
+        if query == 1 {
+            input! {
+                x: char,
+            }
+            s.push_back(x);
+            current *= 10;
+            current += x as u64 - 48;
+            current %= MOD;
+        }
+        if query == 2 {
+            let front = s[0] as u64 - 48;
+            let sub = front * mods[s.len()] % MOD;
+
+            current += MOD;
+            current -= sub;
+            current %= MOD;
+            s.pop_front();
+        }
+        if query == 3 {
+            println!("{}", current % MOD);
+        }
+    }
+}
