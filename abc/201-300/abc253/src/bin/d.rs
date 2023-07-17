aff528f8b26be36cce02950d5ee80239565102ae
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2023-07-17 19:23:41 +0900

    implemented abc253-d

--- abc/201-300/abc253/src/bin/d.rs
+++ abc/201-300/abc253/src/bin/d.rs
@@ -1,17 +1,38 @@
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use std::collections::BinaryHeap;
-// use proconio::marker::Chars;
 use proconio::input;
+
+fn gcd(x: usize, y: usize) -> usize {
+    let mut xy = (x, y);
+    while xy.0 >= 1 && xy.1 >= 1 {
+        if xy.0 < xy.1 {
+            xy.1 %= xy.0;
+        } else {
+            xy.0 %= xy.1;
+        }
+    }
+    if xy.0 >= 1 {
+        xy.0
+    } else {
+        xy.1
+    }
+}
+
+fn lcm(x: usize, y: usize) -> usize {
+    let d = gcd(x,y);
+    x / d * y
+}
+
+fn souwa(num: usize, coeficient: usize) -> usize {
+    ((1+num) * num * coeficient) / 2
+}
 
 fn main() {
     input! {
         n: usize,
-        // (h,w): (usize, usize),
-        // s: Chars,
-        // a: [usize; h],
+        a: usize,
+        b: usize,
     }
-    println!("Yes");
+    let koubaisu = lcm(a, b);
+    let ans = souwa(n, 1) - (souwa(n / a, a) + souwa(n / b, b) - souwa(n / koubaisu, koubaisu));
+    println!("{}", ans);
 }
 
