a20cd3138f1c5aabf198123fc26ea6b50bb2f102
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2023-07-17 19:24:34 +0900

    implemented abc301-d

--- abc/301-400/abc301/src/bin/d.rs
+++ abc/301-400/abc301/src/bin/d.rs
@@ -1,61 +1,32 @@
 use proconio::input;
 use proconio::marker::Chars;
 
+fn is_large(s: &Vec<char>, n: u64) -> bool {
+    let s_as_string = s.iter().map(|&ch| ch).collect::<String>();
+    let s_as_num = u64::from_str_radix(s_as_string.as_str(), 2).unwrap();
+    s_as_num > n
+}
+
 fn main() {
     input! {
         s: Chars,
         n: u64,
     }
-    let n_vec = format!("{:>060b}", n).chars().collect::<Vec<char>>();
-    let s_vec = (0..60)
-        .map(|i| {
-            if i >= 60 - s.len() {
-                s[i - (60 - s.len())]
-            } else {
-                '0'
-            }
-        })
-        .collect::<Vec<char>>();
-
-
-    let mut max = 0;
-    for i in 0..60 {
-        if n_vec[i] == '1' && s_vec[i] == '?' {
-            max = i;
-        }
+    let mut current = s.iter().map(|&ch| if ch == '?' {'0'} else {ch}).collect::<Vec<char>>();
+    if is_large(&current, n) {
+        println!("{}", -1);
+        return;
     }
-    let is_last_change = (max..60).filter(|&i| n_vec[i] == '0' && s_vec[i] == '1').count() > 0;
-    
-    let mut is_less = false;
-    let mut ans = s_vec.clone();
-    for i in 0..60 {
-        if !is_less && n_vec[i] == '0' && s_vec[i] == '1' {
-            println!("-1");
-            return;
-        }
-        if !is_less && n_vec[i] == '1' && s_vec[i] == '0' {
-            is_less = true;
-        }
 
-        if s_vec[i] == '?' {
-            if is_less {
-                ans[i] = '1';
-            } else {
-                if i == max && is_last_change {
-                    ans[i] = '0';
-                    is_less = true;
-                } else {
-                    ans[i] = n_vec[i];
-                }
+    for i in 0..s.len() {
+        if s[i] == '?' {
+            current[i] = '1';
+            if is_large(&current, n) {
+                current[i] = '0';
             }
         }
     }
-    let ans_string: String = ans.iter().collect();
-    let ans_u64 = u64::from_str_radix(ans_string.as_str(), 2).unwrap();
-    if ans_u64 <= n as u64 {
-        println!("{}", ans_u64);
-    } else {
-        println!("-1");
-    }
+
+    let current_as_string = current.iter().map(|&ch| ch).collect::<String>();
+    println!("{}", u64::from_str_radix(current_as_string.as_str(), 2).unwrap());
 }
-
