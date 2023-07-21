d3fedfce4b07e66418ad2c3271b1b983b26502b2
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2023-07-22 00:26:48 +0900

    implemented abc293-d

--- abc/201-300/abc293/src/bin/d.rs
+++ abc/201-300/abc293/src/bin/d.rs
@@ -1,17 +1,60 @@
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use std::collections::BinaryHeap;
-// use proconio::marker::Chars;
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        // (h,w): (usize, usize),
-        // s: Chars,
-        // a: [usize; h],
+        m: usize,
     }
-    println!("Yes");
+    let mut graph = vec![vec![0, 0]; n+1];
+
+    for _ in 0..m {
+        input! {
+            a: usize,
+            b: char,
+            c: usize,
+            d: char,
+        }
+        if b == 'R' && d == 'R' {
+            graph[a][0] = c;
+            graph[c][0] = a;
+        } else if b == 'R' && d == 'B' {
+            graph[a][0] = c;
+            graph[c][1] = a;
+        } else if b == 'B' && d == 'R' {
+            graph[a][1] = c;
+            graph[c][0] = a;
+        } else if b == 'B' && d == 'B' {
+            graph[a][1] = c;
+            graph[c][1] = a;
+        }
+        
+    }
+
+    let mut visited = vec![false; n+1];
+    let (mut x, mut y) = (0, 0);
+    for i in 1..n+1 {
+        if !visited[i] {
+            let mut flag = true;
+            dfs(&mut visited, &graph, i, &mut flag);
+            if flag {
+                x += 1;
+            } else {
+                y += 1;
+            }
+        }
+    }
+    println!("{} {}", x, y);
+}
+
+fn dfs(visited: &mut Vec<bool>, graph: &Vec<Vec<usize>>, pos: usize, flag: &mut bool) {
+    if graph[pos][0] == 0 || graph[pos][1] == 0 {
+        *flag = false;
+    }
+    visited[pos] = true;
+    for point in graph[pos].iter() {
+        if visited[*point] == false {
+            dfs(visited, graph, *point, flag);
+        }
+    }
 }
 
