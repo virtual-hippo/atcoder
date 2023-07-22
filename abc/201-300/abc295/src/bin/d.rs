fe25961f1b7a96ec72a0bcb91dccd51e5328ca75
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2023-07-22 12:21:57 +0900

    implemented abc295-d

--- abc/201-300/abc295/src/bin/d.rs
+++ abc/201-300/abc295/src/bin/d.rs
@@ -1,17 +1,26 @@
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use std::collections::BinaryHeap;
-// use proconio::marker::Chars;
 use proconio::input;
+use proconio::marker::Chars;
+use std::collections::HashMap;
 
 fn main() {
     input! {
-        n: usize,
-        // (h,w): (usize, usize),
-        // s: Chars,
-        // a: [usize; h],
+        s: Chars,
     }
-    println!("Yes");
-}
+    let mut table = vec![0; 10];
+    let mut map = HashMap::new();
+    *map.entry(table.clone()).or_insert(0) += 1;
 
+    for i in 0..s.len() {
+        let num = s[i].to_digit(10).unwrap() as usize;
+        table[num] += 1;
+        table[num] %= 2;
+        *map.entry(table.clone()).or_insert(0) += 1;
+    }
+
+    let mut ans: u64 = 0;
+    for &val in map.values() {
+        ans += (val * (val - 1)) / 2;
+    }
+
+    println!("{}", ans);
+}
