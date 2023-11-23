10ce1823d4e0686aada924913c1f841b0afdfef1
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2023-11-23 21:05:37 +0900

    added tessoku-booku-a23

--- tessoku-book/src/bin/a23.rs
+++ tessoku-book/src/bin/a23.rs
@@ -1,17 +1,26 @@
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use std::collections::BinaryHeap;
-// use proconio::marker::Chars;
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n: usize,
-        // (h,w): (usize, usize),
-        // s: Chars,
-        // a: [usize; h],
+        m: usize,
+        a: [[usize; n]; m],
     }
-    println!("Yes");
+    let mut dp = vec![vec![usize::MAX - 1; 1 << n]; m + 1];
+    dp[0][0] = 0;
+    for i in 1..m + 1 {
+        for j in 0..(1 << n) {
+            let v = (0..n)
+                .filter(|&k| (j & (1 << k) != 0) || a[i - 1][k] == 1)
+                .fold(0, |sum, k| sum + (1 << k));
+            dp[i][j] = dp[i][j].min(dp[i - 1][j]);
+            dp[i][v] = dp[i][v].min(dp[i - 1][j] + 1);
+        }
+    }
+    if dp[m][(1 << n) - 1] == usize::MAX - 1 {
+        println!("-1");
+    } else {
+        println!("{}", dp[m][(1 << n) - 1]);
+    }
 }
-
