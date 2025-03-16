ee667cb5206065c68512f8e328edc7cc005b6f24
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2025-03-16 15:30:03 +0900

    Implemented tessoku-book-b65

--- tessoku-book/src/bin/b65.rs
+++ tessoku-book/src/bin/b65.rs
@@ -1,17 +1,45 @@
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use std::collections::BinaryHeap;
-// use proconio::marker::Chars;
-use proconio::input;
+#![allow(unused_imports)]
+use ac_library::*;
+use itertools::*;
+use proconio::{fastout, input, marker::Chars};
+use rustc_hash::{FxHashMap, FxHashSet};
+use std::{collections::VecDeque, usize};
+use superslice::Ext;
 
+#[fastout]
 fn main() {
     input! {
         n: usize,
-        // (h,w): (usize, usize),
-        // s: Chars,
-        // a: [usize; h],
+        t: usize,
     }
-    println!("Yes");
+    let t = t - 1;
+    let tree = {
+        let mut res = vec![vec![]; n];
+
+        for _ in 0..n - 1 {
+            input! {
+                a: usize,
+                b: usize,
+            }
+            let a = a - 1;
+            let b = b - 1;
+            res[a].push(b);
+            res[b].push(a);
+        }
+        res
+    };
+
+    let mut ans = vec![0; n];
+    dfs(t, usize::MAX, &tree, &mut ans);
+    let ans = ans.iter().map(|v| v.to_string()).join(" ");
+    println!("{}", ans);
 }
 
+fn dfs(v: usize, p: usize, tree: &Vec<Vec<usize>>, ans: &mut Vec<usize>) -> usize {
+    let mut mx = 0;
+    for &u in tree[v].iter().filter(|&&u| u != p) {
+        mx = mx.max(dfs(u, v, tree, ans));
+    }
+    ans[v] = mx;
+    mx + 1
+}
