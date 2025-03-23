6b8a53fae73a478e530fad6e8dc7a555bbd525f8
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2025-03-23 17:49:19 +0900

    Added rolling_hash to lib

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -2,16 +2,4 @@ pub mod binary_search;
 pub mod gcd;
 pub mod mint;
 pub mod prime_factorize;
-
-// fn _dfs(visited: &mut HashMap<u32, bool>, graph: &HashMap::<u32, Vec<u32>>, pos: u32) {
-//     visited.entry(pos).and_modify(|v| *v = true);
-//     if let Some(vec) = graph.get(&pos) {
-//         for i in vec.iter() {
-//             if let Some(b) = visited.get(i) {
-//                 if *b == false {
-//                     dfs(visited, graph, *i);
-//                 }
-//             }
-//         }
-//     }
-// }
+pub mod rolling_hash;
