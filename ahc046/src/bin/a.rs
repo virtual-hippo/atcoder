b57398bbb456d338cfb8175bdf8c55e1950b3571
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2025-04-26 18:45:55 +0900

    [ahc046] snd sub

--- ahc046/src/bin/a.rs
+++ ahc046/src/bin/a.rs
@@ -2,6 +2,7 @@
 use ac_library::*;
 use itertools::*;
 use proconio::{fastout, input, marker::Chars};
+use rand::Rng;
 use rustc_hash::{FxHashMap, FxHashSet};
 use std::time::{Duration, Instant};
 use std::{collections::VecDeque, usize};
@@ -95,25 +96,24 @@ struct Info {
     time_limit: Duration,
     best_answer: (usize, Vec<Action>), // 最適解
     candidate: FxHashMap<Pos, usize>,  // 壁の建設候補
+    rng: rand::rngs::ThreadRng,
 }
 
 impl Info {
     fn new(input: &Input) -> Self {
-        let state = vec![vec!['.'; input.n]; input.n];
-        let time_limit = Duration::from_millis(1935);
-        let start_time = Instant::now();
-
+        let rng = rand::prelude::ThreadRng::default();
         Self {
-            state,
+            state: vec![vec!['.'; input.n]; input.n],
             next: 1,
             last_visited: 0,
             now_pos: input.goal[0],
             action_hisotry: vec![],
             position_hisotry: vec![],
-            time_limit: Duration::from_millis(1935),
+            time_limit: Duration::from_millis(1920),
             start_time: Instant::now(),
             best_answer: (0, vec![]),
             candidate: FxHashMap::default(),
+            rng,
         }
     }
 
@@ -123,9 +123,20 @@ impl Info {
 
     fn update_best_answer(&mut self) {
         let new_score = calculate_score(self);
+        eprintln!("new score: {}", new_score);
         if new_score > self.best_answer.0 {
             self.best_answer = (new_score, self.action_hisotry.clone());
         }
+    }
+
+    fn clear(&mut self, input: &Input) {
+        self.state = vec![vec!['.'; self.state.len()]; self.state.len()];
+        self.next = 1;
+        self.last_visited = 0;
+        self.now_pos = input.goal[0];
+        self.action_hisotry.clear();
+        self.position_hisotry.clear();
+        // self.candidate.clear();
     }
 }
 
@@ -141,7 +152,7 @@ fn calculate_score(info: &Info) -> usize {
 }
 
 fn print_result(info: &Info) {
-    for action in &info.action_hisotry {
+    for action in info.best_answer.1.iter() {
         println!("{}", action);
     }
 }
@@ -193,6 +204,49 @@ fn find_latest_wall(dir: &Dir, info: &Info) -> Pos {
             }
         },
     }
+}
+
+fn select_building_wall(input: &Input, info: &mut Info, action: Action, now: Pos) -> Option<Action> {
+    let dir = match action {
+        Action::Move(dir) => dir,
+        Action::Slide(dir) => dir,
+        _ => unreachable!(),
+    };
+
+    let neighboring_positions = [
+        (Dir::Up, Pos { x: now.x.saturating_sub(1), y: now.y }),
+        (
+            Dir::Down,
+            Pos {
+                x: (now.x + 1).min(input.n - 1),
+                y: now.y,
+            },
+        ),
+        (Dir::Left, Pos { x: now.x, y: now.y.saturating_sub(1) }),
+        (
+            Dir::Right,
+            Pos {
+                x: now.x,
+                y: (now.y + 1).min(input.n - 1),
+            },
+        ),
+    ]
+    .iter()
+    .filter(|&(_, pos)| pos != &now)
+    .filter(|&(n_dir, _)| *n_dir != dir)
+    .map(|(_, pos)| *pos)
+    .collect::<Vec<_>>();
+
+    // 壁の建設候補地が上下左右にある場合は、壁を建設する
+    let rnd_val = info.rng.gen_range(0..100);
+    for pos in neighboring_positions {
+        if let Some(goal) = info.candidate.get(&pos) {
+            if info.state[pos.x][pos.y] == '.' && goal > &info.last_visited && rnd_val < 25 {
+                return Some(Action::Apply(dir.clone()));
+            }
+        }
+    }
+    None
 }
 
 /// 最適な行動を選択する
@@ -275,6 +329,13 @@ fn select_action(input: &Input, info: &mut Info, next_goal: &Pos) -> Option<Acti
         }
     }
 
+    if let Some(action) = &best_action {
+        // 壁の建設候補地が上下左右にある場合は、壁を建設する
+        if let Some(build_wall_action) = select_building_wall(input, info, action.clone(), now) {
+            return Some(build_wall_action);
+        }
+    }
+
     best_action
 }
 
@@ -282,10 +343,7 @@ fn select_action(input: &Input, info: &mut Info, next_goal: &Pos) -> Option<Acti
 /// 1. Move: 上下左右どれかに1マスだけ移動する
 /// 2. Slide: 上下左右どれかに可能な限り移動する (現在の座標 (x,y) とすると (0,y) or (x,0) or (n,y) or (x,n) となるように移動する)
 fn do_best_action(input: &Input, info: &mut Info, next_goal: &Pos) {
-    let best_action = select_action(input, info, next_goal);
-
-    // Execute the best action
-    if let Some(action) = best_action {
+    if let Some(action) = select_action(input, info, next_goal) {
         match &action {
             Action::Move(dir) => match dir {
                 Dir::Up => info.now_pos.x -= 1,
@@ -299,15 +357,28 @@ fn do_best_action(input: &Input, info: &mut Info, next_goal: &Pos) {
                 Dir::Left => info.now_pos.y = 0,
                 Dir::Right => info.now_pos.y = input.n - 1,
             },
-            _ => {},
+            Action::Apply(dir) => {
+                let pos = match dir {
+                    Dir::Up => Pos { x: info.now_pos.x - 1, y: info.now_pos.y },
+                    Dir::Down => Pos { x: info.now_pos.x + 1, y: info.now_pos.y },
+                    Dir::Left => Pos { x: info.now_pos.x, y: info.now_pos.y - 1 },
+                    Dir::Right => Pos { x: info.now_pos.x, y: info.now_pos.y + 1 },
+                };
+                if info.state[pos.x][pos.y] == '#' {
+                    info.state[pos.x][pos.y] = '.';
+                } else {
+                    info.state[pos.x][pos.y] = '#';
+                }
+            },
         }
+
         // eprintln!("Now: {} {}", info.now_pos.x, info.now_pos.y);
         info.position_hisotry.push(info.now_pos);
         info.action_hisotry.push(action);
     }
 }
 
-fn create_initial_answer(input: &Input, info: &mut Info) {
+fn create_answer(input: &Input, info: &mut Info) {
     while info.action_hisotry.len() < 2 * input.n * input.m && info.next < input.m {
         let next_goal = input.goal[info.next];
         do_best_action(input, info, &next_goal);
@@ -426,12 +497,19 @@ fn create_candidate_to_build_wall(input: &Input, info: &mut Info) {
 
 fn solve(input: &Input, info: &mut Info) {
     // 初期解作成
-    create_initial_answer(input, info);
+    create_answer(input, info);
 
     // 壁の建設候補を作成
     create_candidate_to_build_wall(input, info);
 
+    while !info.is_time_up() {
+        create_answer(input, info);
+        info.update_best_answer();
+        info.clear(input);
+    }
+
     print_result(info);
+    eprintln!("best score: {}", info.best_answer.0);
 }
 
 #[fastout]
