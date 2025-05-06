ad3d850f154ed18336c94ebdbb91f822a9bdf099
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2025-05-06 18:22:30 +0900

    Implemented EDPC-f

--- dp/src/bin/f.rs
+++ dp/src/bin/f.rs
@@ -1,18 +1,59 @@
-#![allow(unused_imports)]
-use ac_library::*;
-use itertools::*;
 use proconio::{fastout, input, marker::Chars};
-use rustc_hash::{FxHashMap, FxHashSet};
-use std::collections::{VecDeque, HashMap, HashSet};
-use superslice::Ext;
 
 #[fastout]
 fn main() {
     input! {
-        n: usize,
+        s: Chars,
+        t: Chars,
+    }
+    let sn = s.len();
+    let tn = t.len();
+
+    let mut dp = vec![vec![0; tn + 1]; sn + 1];
+
+    for i in 0..sn + 1 {
+        for j in 0..tn + 1 {
+            if i > 0 {
+                dp[i][j] = dp[i][j].max(dp[i - 1][j]);
+            }
+
+            if j > 0 {
+                dp[i][j] = dp[i][j].max(dp[i][j - 1]);
+            }
+
+            if i > 0 && j > 0 && s[i - 1] == t[j - 1] {
+                dp[i][j] = dp[i][j].max(dp[i - 1][j - 1] + 1);
+            }
+        }
+    }
+    let (mut i, mut j) = (sn, tn);
+    let mut ans = vec![];
+    while i > 0 && j > 0 {
+        if i > 0 && j > 0 && s[i - 1] == t[j - 1] {
+            ans.push(s[i - 1]);
+            i -= 1;
+            j -= 1;
+            continue;
+        }
+        if i > 0 && j > 0 {
+            if dp[i - 1][j] > dp[i][j - 1] {
+                i -= 1;
+            } else {
+                j -= 1;
+            }
+            continue;
+        }
+        if i > 0 {
+            i -= 1;
+            continue;
+        }
+        if j > 0 {
+            j -= 1;
+            continue;
+        }
     }
 
-    let ans = 0;
+    let ans = ans.iter().rev().collect::<String>();
+
     println!("{}", ans);
 }
-
