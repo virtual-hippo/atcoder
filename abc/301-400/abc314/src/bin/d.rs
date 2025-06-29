7309383fd4d09186788e226aa9c25df7dd9ea690
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2025-06-29 23:47:00 +0900

    Implemented abc314-d

--- abc/301-400/abc314/src/bin/d.rs
+++ abc/301-400/abc314/src/bin/d.rs
@@ -1,18 +1,59 @@
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use std::collections::BinaryHeap;
-// use proconio::marker::Chars;
-use proconio::input;
+use proconio::{fastout, input, marker::Chars};
+use std::collections::HashSet;
 
+#[fastout]
 fn main() {
     input! {
         n: usize,
-        // (h,w): (usize, usize),
-        // s: Chars,
-        // a: [usize; h],
+        mut s: Chars,
+        q: usize,
     }
-    let ans = 0;
+
+    let mut set = HashSet::new();
+    for i in 0..n {
+        set.insert(i);
+    }
+
+    let mut is_lower = 0;
+
+    for _ in 0..q {
+        input! {
+            t: usize,
+            x: usize,
+            c: char,
+        }
+
+        match t {
+            1 => {
+                let idx = x - 1;
+                s[idx] = c;
+                set.insert(idx);
+            },
+            2 => {
+                is_lower = 1;
+                set = HashSet::new();
+            },
+            3 => {
+                is_lower = 2;
+                set = HashSet::new();
+            },
+            _ => unreachable!(),
+        }
+    }
+
+    let ans = if is_lower == 1 {
+        s.iter()
+            .enumerate()
+            .map(|(i, &c)| if set.contains(&i) { c } else { c.to_ascii_lowercase() })
+            .collect::<String>()
+    } else if is_lower == 2 {
+        s.iter()
+            .enumerate()
+            .map(|(i, &c)| if set.contains(&i) { c } else { c.to_ascii_uppercase() })
+            .collect::<String>()
+    } else {
+        s.iter().map(|&c| c).collect::<String>()
+    };
+
     println!("{}", ans);
 }
-
