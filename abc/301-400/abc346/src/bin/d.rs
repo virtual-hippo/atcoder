101f531fa46268c3e58b87548f72860ebeccd251
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2026-01-03 02:17:58 +0900

    updated abc346-d

--- abc/301-400/abc346/src/bin/d.rs
+++ abc/301-400/abc346/src/bin/d.rs
@@ -38,3 +38,55 @@ fn main() {
 
     println!("{}", ans);
 }
+
+// https://publish.obsidian.md/naoya/articles/%E3%80%8C%E3%83%95%E3%82%A7%E3%83%BC%E3%82%BA%E3%80%8D%E3%82%92%E7%8A%B6%E6%85%8B%E3%81%AB%E3%81%99%E3%82%8BDP
+pub fn solve() {
+    input! {
+        n: usize,
+        s: String,
+        c: [usize; n],
+    }
+    let s = s.chars().map(|c| (c as u8 - b'0') as usize).collect_vec();
+
+    let mut init = [[usize::MAX; 2]; 2];
+    init[0][s[0]] = 0;
+    init[0][s[0] ^ 1] = c[0];
+
+    let dp = (1..n).fold(init, |old, i| {
+        let mut dp = [[usize::MAX; 2]; 2];
+
+        let cost = |to: usize| -> usize {
+            if s[i] == to {
+                0
+            } else {
+                c[i]
+            }
+        };
+
+        // before -> before (0 -> 1 or 1 -> 0)
+        for to in 0..2 {
+            if old[0][to ^ 1] != usize::MAX {
+                dp[0][to] = old[0][to ^ 1] + cost(to);
+            }
+        }
+
+        // before -> after (0 -> 0 or 1 -> 1)
+        for to in 0..2 {
+            if old[0][to] != usize::MAX {
+                dp[1][to] = dp[1][to].min(old[0][to] + cost(to));
+            }
+        }
+
+        // after -> after (0 -> 1 or 1 -> 0)
+        for to in 0..2 {
+            if old[1][to ^ 1] != usize::MAX {
+                dp[1][to] = dp[1][to].min(old[1][to ^ 1] + cost(to));
+            }
+        }
+
+        dp
+    });
+
+    let ans = dp[1][0].min(dp[1][1]);
+    println!("{}", ans);
+}
