597d6f4f411f64b47a158b2c4e3dadf826d2bb4a
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2026-01-04 02:08:44 +0900

    updated abc324-d

--- abc/301-400/abc324/src/bin/d.rs
+++ abc/301-400/abc324/src/bin/d.rs
@@ -1,25 +1,47 @@
 use proconio::input;
-use proconio::marker::Chars;
+
+type Bucket = [u8; 10];
+
+fn digit_counts(mut x: u64) -> Bucket {
+    let mut counts = [0u8; 10];
+    if x == 0 {
+        counts[0] = 1;
+    } else {
+        while x > 0 {
+            counts[(x % 10) as usize] += 1;
+            x /= 10;
+        }
+    }
+    counts
+}
 
 fn main() {
     input! {
         n: usize,
-        mut s: Chars,
+        s: String,
     }
-    s.sort();
+    let bucket = s.chars().map(|ch| (ch as u8 - b'0')).fold([0; 10], |mut acc: Bucket, x: u8| {
+        acc[x as usize] += 1;
+        acc
+    });
 
-    let mut ans = 0_u64;
-    let mut i = 0;
-    while i * i < 10_u64.pow(n as u32) {
-        let mut cur = (i * i).to_string().chars().collect::<Vec<char>>();
-        while cur.len() < s.len() {
-            cur.push('0');
-        }
-        cur.sort();
-        if s == cur {
-            ans += 1;
-        }
-        i += 1;
-    }
-    println!("{ans}")
+    let mx = 10_u64.pow(n as u32);
+
+    let ans = (0..)
+        .take_while(|x| x * x < mx)
+        .map(|x| x * x)
+        .map(|x| {
+            let digits_diff = n - if x == 0 { 1 } else { x.ilog10() as usize + 1 };
+            if digits_diff > 0 {
+                let mut b = digit_counts(x);
+                b[0] += digits_diff as u8;
+                b
+            } else {
+                digit_counts(x)
+            }
+        })
+        .filter(|xs| *xs == bucket)
+        .count();
+
+    println!("{}", ans);
 }
