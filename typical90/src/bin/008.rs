336a6c15bea19f8d6fb6e08c0ef385246d41e63e
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2026-01-03 18:01:52 +0900

    implemented typical90-008

--- typical90/src/bin/008.rs
+++ typical90/src/bin/008.rs
@@ -1,11 +1,21 @@
+use ac_library::*;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        n: usize,
+        _n: usize,
+        s: String,
     }
-    let ans = 0;
+
+    const ATCODER: [char; 8] = [' ', 'a', 't', 'c', 'o', 'd', 'e', 'r'];
+
+    let mut init = [ModInt1000000007::new(0); ATCODER.len()];
+    init[0] += 1;
+
+    let dp = s.chars().fold(init, |dp, ch| {
+        std::array::from_fn(|j| if j > 0 && ch == ATCODER[j] { dp[j] + dp[j - 1] } else { dp[j] })
+    });
+    let ans = dp[7];
     println!("{}", ans);
 }
-
