22a458c147067d7c48bb041d0e29d62bd27c5a62
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2026-01-04 00:40:43 +0900

    Implemented tessoku-book b24

--- tessoku-book/src/bin/b24.rs
+++ tessoku-book/src/bin/b24.rs
@@ -1,17 +1,28 @@
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use std::collections::BinaryHeap;
-// use proconio::marker::Chars;
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        // (h,w): (usize, usize),
-        // s: Chars,
-        // a: [usize; h],
+        xy: [(usize, usize); n],
     }
-    println!("Yes");
-}
+    let xy = xy
+        .iter()
+        .copied()
+        .sorted_by_key(|&(x, y)| (x, std::cmp::Reverse(y)))
+        .collect_vec();
 
+    let dp = (0..n).fold(vec![], |mut dp, i| {
+        let (_, y) = xy[i];
+
+        let pos = dp.partition_point(|&v| v < y);
+        if pos == dp.len() {
+            dp.push(y);
+        } else {
+            dp[pos] = y;
+        }
+
+        dp
+    });
+    println!("{}", dp.len());
+}
