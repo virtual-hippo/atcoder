e52780cc617ed23daa6a69c901229742b455e56e
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2026-01-04 21:55:25 +0900

    updated abc330-c

--- abc/301-400/abc330/src/bin/c.rs
+++ abc/301-400/abc330/src/bin/c.rs
@@ -3,16 +3,20 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        d: i64,
+        d: u64,
     }
-    let mut x = 0;
-    let mut ans = i64::MAX;
-    while x * x <= d {
-        let y = ((d - x * x) as f64).sqrt() as i64;
-        for yy in y - 3..y + 4 {
-            ans = ans.min((x * x + yy * yy - d).abs());
-        }
-        x += 1;
-    }
+
+    let ans = (0..)
+        .take_while(|&x| x * x <= d)
+        .map(|x| {
+            let yy = d - x.pow(2);
+            let y0 = yy.isqrt();
+            let y1 = y0 + 1;
+
+            ((x.pow(2) + y0.pow(2)).abs_diff(d)).min((x.pow(2) + y1.pow(2)).abs_diff(d))
+        })
+        .min()
+        .unwrap();
+
     println!("{}", ans);
 }
