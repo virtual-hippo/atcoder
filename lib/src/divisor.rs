1ba03f1084ff5b827eaa50c0c04c9cae88702223
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2026-01-05 18:03:42 +0900

    Renamed divisor

--- lib/src/divisor.rs
+++ lib/src/divisor.rs
@@ -3,11 +3,11 @@ use itertools::*;
 /// 約数一覧を取得
 ///
 /// ```
-/// use abclib::prime::get_primes;
+/// use abclib::divisor::get_divisors;
 ///
-/// assert_eq!(get_primes(6), vec![1,2,3,6]);
+/// assert_eq!(get_divisors(6), vec![1,2,3,6]);
 /// ```
-pub fn get_primes(n: usize) -> Vec<usize> {
+pub fn get_divisors(n: usize) -> Vec<usize> {
     (1_usize..)
         .take_while(|&x| x.pow(2) <= n)
         .filter(|&x| n % x == 0)
