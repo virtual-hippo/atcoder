2f59d5414273964aba8716e406f96b53b97f2b7c
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2026-01-05 21:01:55 +0900

    updated abc296-d

--- abc/201-300/abc296/src/bin/d.rs
+++ abc/201-300/abc296/src/bin/d.rs
@@ -1,24 +1,27 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
         n: usize,
         m: usize,
     }
-    let mut ans = std::usize::MAX;
-    for a in 1..n+1 {
-        let b = (m+a-1) / a;
-        if b <= n {
-            ans = ans.min(a * b);
-        }
-        if a > b {
-            break;
-        }
-    }
-    if ans == std::usize::MAX {
-        println!("{}", -1);
+
+    let ans = (1_usize..)
+        .take_while(|&a| a <= (m.isqrt() + 1))
+        .filter_map(|a| {
+            let b = if m / a * a >= m { m / a } else { m / a + 1 };
+            if a <= n && b <= n {
+                Some(a * b)
+            } else {
+                None
+            }
+        })
+        .min();
+
+    if let Some(ans) = ans {
+        println!("{}", ans);
     } else {
-        println!("{}", ans)
+        println!("{}", -1);
     }
 }
-
