6dc0b74f81a30cf87691aab05a9d07e6f6ed75cc
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2026-01-05 22:19:58 +0900

    Implemented tessoku-book b57

--- tessoku-book/src/bin/b57.rs
+++ tessoku-book/src/bin/b57.rs
@@ -1,17 +1,42 @@
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use std::collections::BinaryHeap;
-// use proconio::marker::Chars;
+use itertools::*;
 use proconio::input;
 
+pub fn digits_to_vec(x: usize) -> Vec<usize> {
+    if x / 10 == 0 {
+        vec![x % 10]
+    } else {
+        digits_to_vec(x / 10).into_iter().chain(std::iter::once(x % 10)).collect()
+    }
+}
+
+fn f(x: usize) -> usize {
+    x - digits_to_vec(x).iter().sum::<usize>()
+}
+
+// ダブリング
 fn main() {
     input! {
         n: usize,
-        // (h,w): (usize, usize),
-        // s: Chars,
-        // a: [usize; h],
+        k: usize,
     }
-    println!("Yes");
-}
 
+    let m = 30;
+    let r = n + 1;
+
+    let init = vec![(0..r).map(f).collect_vec()];
+
+    let dp = (1..m).fold(init, |mut dp, _| {
+        let pre = dp.len() - 1;
+        let new = (0..r).map(|i| dp[pre][dp[pre][i]]).collect_vec();
+        dp.push(new);
+        dp
+    });
+
+    for i in 1..r {
+        let ans = (0..m)
+            .rev()
+            .filter(|&d| (k / (1_usize << d)) % 2 != 0)
+            .fold(i, |now, d| dp[d][now]);
+        println!("{}", ans);
+    }
+}
