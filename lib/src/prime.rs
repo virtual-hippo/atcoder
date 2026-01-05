0ce7b9d01d6c0fdf9d1e58dfecc88233b977d060
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2026-01-05 17:54:40 +0900

    Updated get_primes()

--- lib/src/prime.rs
+++ lib/src/prime.rs
@@ -1,15 +1,18 @@
+use itertools::*;
+
 /// 約数一覧を取得
+///
+/// ```
+/// use abclib::prime::get_primes;
+///
+/// assert_eq!(get_primes(6), vec![1,2,3,6]);
+/// ```
 pub fn get_primes(n: usize) -> Vec<usize> {
-    let mut i = 1;
-    let mut ret = vec![];
-    while i * i <= n {
-        if i * i == n {
-            ret.push(i);
-        } else if n % i == 0 {
-            ret.push(i);
-            ret.push(n / i);
-        }
-        i += 1;
-    }
-    ret
+    (1_usize..)
+        .take_while(|&x| x.pow(2) <= n)
+        .filter(|&x| n % x == 0)
+        .map(|x| if x * x == n { vec![x] } else { vec![x, n / x] })
+        .flatten()
+        .sorted()
+        .collect_vec()
 }
