d21d88b7a8a380548130946b85cb49ace11fe262
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2026-01-05 21:44:18 +0900

    Implemented tessoku-book a57

--- tessoku-book/src/bin/a57.rs
+++ tessoku-book/src/bin/a57.rs
@@ -1,17 +1,30 @@
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use std::collections::BinaryHeap;
-// use proconio::marker::Chars;
-use proconio::input;
+use itertools::*;
+use proconio::{input, marker::*};
+
+// ダブリング
 
 fn main() {
     input! {
         n: usize,
-        // (h,w): (usize, usize),
-        // s: Chars,
-        // a: [usize; h],
+        q: usize,
+        a: [Usize1; n],
     }
-    println!("Yes");
-}
 
+    let dp = (1..32).fold(vec![a.clone()], |mut dp, _| {
+        let pre = dp.len() - 1;
+        let new = (0..n).map(|i| dp[pre][dp[pre][i]]).collect_vec();
+        dp.push(new);
+        dp
+    });
+
+    for _ in 0..q {
+        input! {
+            x: Usize1,
+            y: usize,
+        }
+
+        let ans = (0..30).rev().filter(|&j| (y / (1 << j)) % 2 != 0).fold(x, |now, d| dp[d][now]) + 1;
+
+        println!("{}", ans);
+    }
+}
