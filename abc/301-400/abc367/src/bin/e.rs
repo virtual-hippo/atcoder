c22342c9efe6a875bd9beb8a0fee6c47a585a504
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2026-01-05 23:39:58 +0900

    updated abc367-e

--- abc/301-400/abc367/src/bin/e.rs
+++ abc/301-400/abc367/src/bin/e.rs
@@ -1,54 +1,37 @@
-#![allow(unused_imports)]
-use ac_library::*;
 use itertools::*;
-use proconio::{fastout, input, marker::Chars};
-use rustc_hash::{FxHashMap, FxHashSet};
-use std::collections::VecDeque;
-use superslice::Ext;
+use proconio::{fastout, input, marker::*};
 
+// ダブリング
 #[fastout]
 fn main() {
     input! {
         n: usize,
         k: usize,
+        x: [Usize1; n],
+        a: [usize; n],
     }
 
-    let mut p = vec![vec![0; n + 1]; 60];
-    for i in 1..=n {
-        input! {
-            x: usize,
-        }
-        p[0][i] = x;
-    }
-    let a = {
-        let mut ret = vec![0; n + 1];
-        for i in 1..=n {
-            input! {
-                a: usize,
-            }
-            ret[i] = a;
-        }
-        ret
-    };
+    let m = 60;
 
-    for lv in 1..60 {
-        for i in 1..=n {
-            p[lv][i] = p[lv - 1][p[lv - 1][i]];
-        }
-    }
+    let dp = (1..m).fold(vec![x], |mut dp, _| {
+        let pre = dp.len() - 1;
+        let new = (0..n).map(|i| dp[pre][dp[pre][i]]).collect_vec();
+        dp.push(new);
+        dp
+    });
 
-    let mut q = (0..n + 1).collect_vec();
-    let mut k = k;
-    for lv in 0..60 {
-        if k % 2 == 1 {
-            for i in 1..=n {
-                q[i] = p[lv][q[i]];
-            }
-        }
-        k /= 2;
-    }
+    let ans = (0..n)
+        .map(|i| (0..m).rev().filter(|&j| k >> j & 1 == 1).fold(i, |acc, j| dp[j][acc]))
+        .map(|i| a[i])
+        .collect_vec();
 
-    for i in 1..=n {
-        print!("{} ", a[q[i]]);
-    }
+    print_vec_1line(&ans);
+}
+
+// ------------------------------------------------------------------------------------------------
+// libs
+// ------------------------------------------------------------------------------------------------
+pub fn print_vec_1line<T: std::fmt::Display>(arr: &[T]) {
+    let msg = arr.iter().map(|x| format!("{}", x)).join(" ");
+    println!("{}", msg);
 }
