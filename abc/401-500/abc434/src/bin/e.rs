859f19c3582d2a457201ad40a5dfc9504a7626e0
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2026-01-24 14:08:29 +0900

    implement abc434-e

--- abc/401-500/abc434/src/bin/e.rs
+++ abc/401-500/abc434/src/bin/e.rs
@@ -1,10 +1,19 @@
-#![allow(unused_imports)]
-use ac_library::*;
-use itertools::*;
-use proconio::{fastout, input, marker::Chars};
-use rustc_hash::{FxHashMap, FxHashSet};
-use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
-use superslice::Ext;
+use proconio::{fastout, input};
+use rustc_hash::{FxBuildHasher, FxHashMap, FxHashSet};
+use std::collections::*;
+
+fn dfs(to: &HashMap<i64, Vec<i64>, FxBuildHasher>, used: &mut HashSet<i64, FxBuildHasher>, u: i64, n: &mut usize, m: &mut usize) {
+    used.insert(u);
+    *n += 1;
+
+    for &v in to[&u].iter() {
+        *m += 1;
+        if used.contains(&v) {
+            continue;
+        }
+        dfs(to, used, v, n, m);
+    }
+}
 
 #[fastout]
 fn main() {
@@ -12,7 +21,33 @@ fn main() {
         n: usize,
     }
 
-    let ans = 0;
+    let mut to = FxHashMap::default();
+    for _ in 0..n {
+        input! {
+            x: i64,
+            r: i64,
+        }
+
+        let u = x - r;
+        let v = x + r;
+        to.entry(u).or_insert_with(|| vec![]).push(v);
+        to.entry(v).or_insert_with(|| vec![]).push(u);
+    }
+
+    let mut used = FxHashSet::default();
+    let mut ans = 0;
+
+    for &u in to.keys() {
+        if used.contains(&u) {
+            continue;
+        }
+        let mut n = 0;
+        let mut m = 0;
+        dfs(&to, &mut used, u, &mut n, &mut m);
+        let m = m / 2;
+
+        ans += if m < n { m } else { n };
+    }
+
     println!("{}", ans);
 }
-
