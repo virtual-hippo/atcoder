e33962519c227a69e3162e39eaf619e814dec70d
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2026-01-24 14:52:01 +0900

    implement abc430-e

--- abc/401-500/abc430/src/bin/e.rs
+++ abc/401-500/abc430/src/bin/e.rs
@@ -1,18 +1,27 @@
-#![allow(unused_imports)]
 use ac_library::*;
-use itertools::*;
-use proconio::{fastout, input, marker::Chars};
-use rustc_hash::{FxHashMap, FxHashSet};
-use std::collections::{VecDeque, HashMap, HashSet};
-use superslice::Ext;
+use proconio::{fastout, input};
+
+// Z-algorithm
+fn solve() {
+    input! {
+        a: String,
+        b: String,
+    }
+    let n = b.len();
+    let c = format!("{}${}{}", b, a, a);
+    let ret = z_algorithm(&c);
+
+    if let Some(i) = (0..n).find(|&i| ret[i + n + 1] == n) {
+        println!("{}", i);
+    } else {
+        println!("-1");
+    }
+}
 
 #[fastout]
 fn main() {
     input! {
-        n: usize,
+        t: usize,
     }
-
-    let ans = 0;
-    println!("{}", ans);
+    (0..t).for_each(|_| solve());
 }
-
