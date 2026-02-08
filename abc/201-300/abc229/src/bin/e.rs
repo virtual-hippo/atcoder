85c6821d09bd4aa6fba4f115157537ebca1d11ca
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2026-02-08 13:51:08 +0900

    Implement abc229-e

--- abc/201-300/abc229/src/bin/e.rs
+++ abc/201-300/abc229/src/bin/e.rs
@@ -1,17 +1,40 @@
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use std::collections::BinaryHeap;
-// use proconio::marker::Chars;
-use proconio::input;
+use ac_library::*;
+use itertools::*;
+use proconio::{fastout, input, marker::*};
 
+#[fastout]
 fn main() {
     input! {
         n: usize,
-        // (h,w): (usize, usize),
-        // s: Chars,
-        // a: [usize; h],
+        m: usize,
+        ab: [(Usize1, Usize1); m],
     }
-    println!("Yes");
-}
 
+    let to = {
+        let mut to = vec![vec![]; n];
+        for i in 0..m {
+            let (a, b) = ab[i];
+            to[a].push(b);
+        }
+        to
+    };
+
+    let mut dsu = Dsu::new(n);
+    let mut cnt = 0;
+    let mut ans = vec![cnt];
+    for a in (1..n).rev() {
+        cnt += 1;
+        for &b in to[a].iter() {
+            if !dsu.same(a, b) {
+                dsu.merge(a, b);
+                cnt -= 1;
+            }
+        }
+        ans.push(cnt);
+    }
+
+    let ans = ans.iter().copied().rev().collect_vec();
+    for i in 0..n {
+        println!("{}", ans[i]);
+    }
+}
